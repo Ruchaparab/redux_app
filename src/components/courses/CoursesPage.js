a36531e8e05c07fe5e533cfd73fbd7b581e7e45c
rucha parab <ruchaparab@gmail.com>
2021-07-13 17:18:00 +0530

    Redux flow for create course

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { connect } from 'react-redux';
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
   state = {
@@ -14,7 +18,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.actions.createCourse(this.state.course);
   }
 
   render() {
@@ -29,4 +33,20 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators( courseActions, dispatch )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
