b1b55531aeb7edde148929a5999688b45e7a3ffd
rucha parab <ruchaparab@gmail.com>
2021-07-14 19:51:37 +0530

    Added Manage courses with Contact form

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { connect } from 'react-redux';
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from 'prop-types';
+import CourseForm from './CourseForm';
+import { newCourse } from '../../../tools/mockData';
+
+function ManageCoursesPage({ courses, authors, loadCourses, loadAuthors, ...props }) {
+
+    const [course, setCourse] = useState({ ...props.course });
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        if (courses.length === 0) {
+            loadCourses().catch(error => {
+                alert("Loading courses failed: " + error);
+            });
+        }
+        if (authors.length === 0) {
+            loadAuthors().catch(error => {
+                alert("Loading authors failed: " + error);
+            });
+        }
+
+    }, [])
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setCourse(prevCourse => ({
+            ...prevCourse,
+            [name]: name === "authorId" ? parseInt(value, 10) : value
+        }))
+    }
+
+    return (
+        <CourseForm course={course} errors={errors} authors={authors} onChange={handleChange} />
+    );
+}
+
+ManageCoursesPage.propTypes = {
+    course: PropTypes.object.isRequired,
+    courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        course: newCourse,
+        courses: state.courses,
+        authors: state.authors
+    }
+}
+
+const mapDispatchToProps = {
+    loadCourses: courseActions.loadCourses,
+    loadAuthors: authorActions.loadAuthors
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursesPage);
