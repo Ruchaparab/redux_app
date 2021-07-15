3040d9764c213174f8bdccec034c99db311e57a1
rucha parab <ruchaparab@gmail.com>
2021-07-15 16:55:41 +0530

    Added spinner,  server side validations

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import CourseList from './CourseList'
-import { Redirect } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
+import Spinner from "../common/Spinner";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
   state = {
     redirectToAddCoursePage: false
-  }
+  };
   componentDidMount() {
     const { courses, authors, actions } = this.props;
 
@@ -24,7 +25,6 @@ class CoursesPage extends React.Component {
         alert("Loading authors failed: " + error);
       });
     }
-
   }
 
   render() {
@@ -32,14 +32,20 @@ class CoursesPage extends React.Component {
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-        <button
-          style={{ marginBottom: 20 }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={this.props.courses} />
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList courses={this.props.courses} />
+          </>
+        )}
       </>
     );
   }
@@ -48,19 +54,26 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-}
+  actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 function mapStateToProps(state) {
   return {
-    courses: state.authors.length === 0 ? [] : state.courses.map(course => {
-      return {
-        ...course,
-        authorName: state.authors.find(author => author.id === course.authorId).name
-      }
-    }),
-    authors: state.authors
-  }
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(
+                author => author.id === course.authorId
+              ).name
+            };
+          }),
+    authors: state.authors,
+    loading: state.apiCallsInProgress > 0
+  };
 }
 
 function mapDispatchToProps(dispatch) {
@@ -69,7 +82,7 @@ function mapDispatchToProps(dispatch) {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
       loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
     }
-  }
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
